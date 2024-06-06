1a8b6f429561d955194568ec0e107c0e61cabb61
Kevin Flaherty <kevco46@gmail.com>
2024-06-06 15:19:56 -0400

    Removed nbsphinx extension

--- docs/conf.py
+++ docs/conf.py
@@ -19,8 +19,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'nbsphinx',
-    'myst_parser'
+    'myst_parser',
 ]
 
 intersphinx_mapping = {
