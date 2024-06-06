d68a416152a341dd5487c9206b76a4c426c73338
Kevin Flaherty <kevco46@gmail.com>
2024-06-06 16:50:59 -0400

    Changed disk.py init function.
    Changed disk.py init function so that it is easier to set one or two parameters while keeping the rest as the default.

--- disk.py
+++ disk.py
@@ -57,10 +57,20 @@ class Disk:
     Tco     = 19.                     # - freeze out
     sigphot = 0.79*sc                 # - photo-dissociation column
 
-    def __init__(self,params=[-0.5,0.09,1.,10.,1000.,150.,51.5,2.3,1e-4,0.01,33.9,19.,69.3,
-                 [.79,1000],[10.,1000],-1],obs=[180,131,300,170],rtg=True,vcs=True,
-                 exp_temp=False,line='co',ring=None,include_selfgrav=False):
+    #def __init__(self,params=[-0.5,0.09,1.,10.,1000.,150.,51.5,2.3,1e-4,0.01,33.9,19.,69.3,
+    #             [.79,1000],[10.,1000],-1],obs=[180,131,300,170],rtg=True,vcs=True,
+    #             exp_temp=False,line='co',ring=None,include_selfgrav=False):
+    def __init__(self,q=-0.5,McoG=0.09,pp=1.,Rin=1.,Rout=1000,Rc=150.,incl=51.5,
+                 Mstar=2.3,Xco=1e-4,vturb=0.01,Zq0=33.9,Tmid0=19,Tatm0=69.3,sigbound=[.79,1000],
+                 Rabund=[10,1000],handed=-1,nr=180,nphi=131,nz=300,zmax=170,rtg=True,vcs=True,
+                 exp_temp=False,line='co',ring=None,include_selfgrav=False,params=[],obs=[] ):
 
+        ### Changed the init function so that it is easier to specify a subset of the parameters
+        ### while keeping the rest as defaults.
+        ### params and obs are kept for some backwards compatability,
+        ### but is does require x=Disk(params=[...]) instead of x=Disk(params)
+        params = [q,McoG,pp,Rin,Rout,Rc,incl,Mstar,Xco,vturb,Zq0,Tmid0,Tatm0,sigbound,Rabund,handed]
+        obs = [nr,nphi,nz,zmax]
         self.ring=ring
         self.set_obs(obs)   # set the observational parameters
         self.set_params(params) # set the structure parameters
