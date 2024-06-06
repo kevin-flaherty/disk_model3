88d25103d549b17cfcf9e93ef0e63ab2cacaf14b
Kevin Flaherty <kevco46@gmail.com>
2024-06-06 11:43:00 -0400

    remove html_theme command

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ on_rtd = os.environ.get("READTHEDOCS",None) == "True"
 if not on_rtd:
     import sphinx_rtd_theme
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]    
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
