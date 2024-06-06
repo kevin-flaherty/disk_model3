f72866465807209f6d75e0d6259f8b727b90a172
Kevin Flaherty <kevco46@gmail.com>
2024-06-06 15:42:30 -0400

    Again, removing myst-parser and nbsphinx

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'nbsphinx'
+    #'nbsphinx',
+    #'myst-parser'
 ]
 
 intersphinx_mapping = {
